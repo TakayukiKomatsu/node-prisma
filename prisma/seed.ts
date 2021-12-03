1384c103e1f026b0f1dc082d09c174b3c7d68d88
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-12-03 16:20:50 -0300

    feat: add seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,26 @@
 import { PrismaClient } from ".prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  await prisma.user.deleteMany({});
+  await prisma.post.deleteMany({});
+
+  const user = await prisma.user.create({
+    data: {
+      name: "Bonieky",
+      email: "suporte@b7web.com.br",
+      age: 70,
+    },
+  });
+
+  const posts = await prisma.post.create({
+    data: {
+      title: "Posts de teste criado via seed",
+      body: "esse é o body",
+      authorId: user.id,
+    },
+  });
+};
+
+main();
